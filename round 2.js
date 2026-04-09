d838c1018eadb7ea1d672b096f38bc88b296a079
srishanth.s <srishanth.selva@gmail.com>
2026-04-09 14:37:18 +0530

    Add files via upload
    done

---
+++ round 2.js
@@ -0,0 +1,15 @@
+
+var a = 5;
+let b = 10;
+const c = 15;
+
+console.log(a, b, c);
+
+console.log(5 == "5");   
+console.log(5 === "5"); 
+
+document.body.innerText = "Hello";
+
+setTimeout(() => {
+  document.body.innerText = "Hi!";
+}, 2000);
